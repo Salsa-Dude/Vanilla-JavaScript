876b2eb9f200ca6d1c09545c5a85c712ed8502dd
Joseph Arias <arias.joseph.a@gmail.com>
2018-03-20 19:07:42 -0400

    final

---
+++ Day 5 - Flexbox/app.js
@@ -0,0 +1,16 @@
+
+const panels = document.querySelectorAll('.panel');
+
+function openToggle() {
+  this.classList.toggle('open');
+}
+
+function openActive(e) {
+  console.log(e.propertyName);
+  if(e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', openToggle));
+panels.forEach(panel => panel.addEventListener('transitionend', openActive));
