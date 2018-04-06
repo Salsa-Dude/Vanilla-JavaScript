bbab18b4f7619e921d98bfac1abc2389faf417fd
Joseph Arias <arias.joseph.a@gmail.com>
2018-04-06 06:30:23 -0400

    Day 6

---
+++ Day 06 - Ajax type ahead/app.js
@@ -0,0 +1,48 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint).then(function(data) {
+  return (data.json());
+}).then(function(citiesData) {
+  cities.push(...citiesData);
+});
+
+
+
+function findMatches(wordToMatch,cities) {
+  return cities.filter(function(place) {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+const numberWithCommas = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  _this = this;
+  const html = matchArray.map(function(place) {
+  const regex = new RegExp(_this.value, 'gi');
+  const cityName = place.city.replace(regex, `<span class="highlight">${_this.value}</span>`);
+  const stateName = place.state.replace(regex, `<span class="highlight">${_this.value}</span>`);
+    return `
+    <li>
+      <span class="name"> ${cityName}, ${stateName}</span>
+      <span class="population"> ${numberWithCommas(place.population)}</span>
+    </li>
+    `;
+  }).join('');
+
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+
